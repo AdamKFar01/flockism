941c7271b69b3f8864828548e571cfbf594aca45
Adam Farhat <akfarhat05@gmail.com>
2026-08-31 21:38:14 +0200

    Added minimal macroquad window with black background

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Flockism".to_owned(),
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        next_frame().await;
+    }
 }
